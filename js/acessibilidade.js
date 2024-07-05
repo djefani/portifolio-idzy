dfc558a83688ed5a4e0fe5a3ab1a69f9f069f5dd
Djéfani Eveeleen <134615080+djefani@users.noreply.github.com>
2024-07-05 18:07:20 -0300

    Add files via upload

---
+++ js/acessibilidade.js
@@ -0,0 +1,9 @@
+
+  (function (d) {
+    var s = d.createElement("script");
+    s.setAttribute("data-account", "GXu4TnQ4yd");
+    s.setAttribute("src", "https://accessibilityserver.org/widget.js");
+    (d.body || d.head).appendChild(s);
+  })(document);
+
+
